f372628e03825d777006e2e27f3c19b92a54c56d
Pedro Palomo <pedro.palomoperez@gmail.com>
2017-01-03 15:16:03 +0000

    first commit

---
+++ api_file-metadata.js
@@ -0,0 +1,35 @@
+var express =   require("express");
+var multer  =   require('multer');
+const http = require('http');
+var app         =   express();
+
+var storage =   multer.diskStorage({
+  destination: function (req, file, callback) {
+    callback(null, './uploads');
+  },
+  filename: function (req, file, callback) {
+    callback(null, file.fieldname + '-' + Date.now());
+  }
+});
+var upload = multer({ storage : storage}).single('userPhoto');
+
+app.get('/',function(req,res){
+      res.sendFile(__dirname + "/index.html");
+});
+
+app.post('/api/photo',function(req,res){
+    
+    upload(req,res,function(err) {
+        if(err) {
+            return res.end("Error uploading file.");
+        }
+        
+        console.log({ size: req.file.size });
+        res.json({ size: req.file.size });
+    });
+});
+
+app.listen(8080,function(){
+    console.log("Working on port", process.env.PORT || 8080);
+});
+
